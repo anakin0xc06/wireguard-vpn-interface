e8848b642bdf87242c976042bd58bc6279da4a5f
Anakin <53931976+anakin0xc06@users.noreply.github.com>
2019-08-24 20:31:31 +0530

    Update commands.go

--- wireguard/commands.go
+++ wireguard/commands.go
@@ -1,32 +1,32 @@
 package wireguard
 
 import (
-  "fmt"
-  "os/exec"
+	"fmt"
+	"os/exec"
 )
 
-func cmdAddDevLink(_inteface, _type string) string {
-  return exec.Command("sh", "-c", fmt.Sprintf("ip link add dev %s type %s", _interface, _type))
+func cmdAddDevLink(_inteface, _type string) *exec.Cmd {
+	return exec.Command("sh", "-c", fmt.Sprintf("ip link add dev %s type %s", _interface, _type))
 }
 
-func cmdDeleteDevLink(_interface string) string {
-  return exec.Command("sh", "-c", fmt.Sprintf("ip link del dev %s", _interface))
+func cmdDeleteDevLink(_interface string) *exec.Cmd {
+	return exec.Command("sh", "-c", fmt.Sprintf("ip link del dev %s", _interface))
 }
 
-func cmdSetDevMTU(_interface string, mtu int) string {
-  return exec.Command("sh", "-c", fmt.Sprintf("ip link set mtu %d up dev %s", mtu, _interface))
+func cmdSetDevMTU(_interface string, mtu int) *exec.Cmd {
+	return exec.Command("sh", "-c", fmt.Sprintf("ip link set mtu %d up dev %s", mtu, _interface))
 }
 
-func cmdAddDevAddressIPv4(_interface, addr string) string {
-  return exec.Command("sh", "-c", fmt.Sprintf("ip -4 address add %s dev %s", addr, _interface))
+func cmdAddDevAddressIPv4(_interface, addr string) *exec.Cmd {
+	return exec.Command("sh", "-c", fmt.Sprintf("ip -4 address add %s dev %s", addr, _interface))
 }
 
-func cmdAddDevAdressIPv6(_interface, addr string) string {
-  return exec.Command("sh", "-c", fmt.Sprintf("ip -6 address add %s dev %s", addr, _interface))
+func cmdAddDevAdressIPv6(_interface, addr string) *exec.Cmd {
+	return exec.Command("sh", "-c", fmt.Sprintf("ip -6 address add %s dev %s", addr, _interface))
 }
 
-func cmdSetNatRouting(_interface string) string {
-  return exec.Command("sh", "-c", fmt.Sprintf(`iptables -C FORWARD -i %s -j ACCEPT &&
+func cmdSetNATRouting(_interface string) *exec.Cmd {
+	return exec.Command("sh", "-c", fmt.Sprintf(`iptables -C FORWARD -i %s -j ACCEPT &&
   iptables -t nat -C POSTROUTING -o eth0 -j MASQUERADE && iptables -C FORWARD -o %s -j ACCEPT`,
-                                              _interface, _interface))
+		_interface, _interface))
 }
